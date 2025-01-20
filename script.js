e639ee8ea29de88ab7015094d2aad7692b90dab6
malwani <98039304+malwani9@users.noreply.github.com>
2025-01-20 16:55:18 +0300

    Add a button that resize grid based on user input

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 const container = document.getElementById("container");
+const gridButton = document.getElementById("grid-button");
 
+let userRows;
+let userColumns;
 
-let rows = 16;
-let columns = 16;
+window.onload = function () {
+  etchASketch();
+};
 
-for(let i = 1; i<=rows; i++){
+gridButton.addEventListener("click", () => {
+  userRows = prompt("Enter number for grid rows: ", "");
+  while (userRows > 100 || userRows < 1) {
+    userRows = prompt("Enter number for grid rows: ", "");
+  }
+
+  userColumns = prompt("Enter number for grid columns", "");
+  while (userColumns > 100 || userColumns < 1) {
+    userColumns = prompt("Enter number for grid columns: ", "");
+  }
+  deleteGridPixels();
+  etchASketch(userRows, userColumns);
+});
+
+function deleteGridPixels() {
+  let containerChilds = document.getElementById("container");
+
+  let child = containerChilds.lastElementChild;
+  while (child) {
+    containerChilds.removeChild(child);
+    child = containerChilds.lastElementChild;
+  }
+}
+
+function etchASketch(rows = 16, columns = 16) {
+  for (let i = 1; i <= rows; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
     container.appendChild(row);
-    for(let j = 1; j <= columns; j++){
-        const pixelDiv = document.createElement("div");
-        pixelDiv.classList.add("pixel");
-        row.appendChild(pixelDiv);
+    for (let j = 1; j <= columns; j++) {
+      const pixelDiv = document.createElement("div");
+      pixelDiv.classList.add("pixel");
+      row.appendChild(pixelDiv);
     }
+  }
 }
-
-
